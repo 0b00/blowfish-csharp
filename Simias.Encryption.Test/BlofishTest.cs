2e5f0245d1ba6c25b7afc4e4b4bc62e78e2afab3
Ishan Jain <ishan@jain.se>
2014-12-05 01:33:25 +0100

    Adding files

---
+++ Simias.Encryption.Test/BlofishTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Simias.Encryption.Test
+{
+    [TestClass]
+    public class BlofishTest
+    {
+        private string salt = "{C42B9D23-F6F8-4AAA-8D1E-00F3A488A507}";
+        private string value = "Test";
+        private string encrypted = "J5APsWH6FOs=";
+
+        [TestMethod]
+        public void EncryptString()
+        {
+            Blowfish blowfish = new Blowfish(salt);
+            Assert.AreEqual(blowfish.Encipher(value), encrypted);
+        }
+        [TestMethod]
+        public void DecryptString()
+        {
+            Blowfish blowfish = new Blowfish(salt);
+            Assert.AreEqual(blowfish.Decipher(encrypted), value);
+        }
+    }
+}
---
+++ Simias.Encryption/ReadMe.txt
@@ -0,0 +1,11 @@
+﻿Note: Only documented usage for strings.
+
+Create a instance of Blowfish object:
+	Blowfish blowfish = new Blowfish(Encoding.Unicode.GetBytes("<Key>"));
+	Blowfish blowfish = new Blowfish("<Key>");
+	
+Encrypt:
+	string encrypted = blowfish.Encipher("<value>");
+
+Decrypt:
+	string value = blowfish.Decipher("<encrypted>");
